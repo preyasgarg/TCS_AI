9a8b19d772e36b9008e6e7867454ad43a317596d
Preyas Garg <preyasgarg01@gmail.com>
2019-08-27 22:01:43 +0530

    Update tcs_ai_face_recogniton.py

--- tcs_ai_face_recogniton.py
+++ tcs_ai_face_recogniton.py
@@ -64,43 +64,38 @@ if os.path.isdir('mathais_face_detected'):
 os.mkdir('mathais_face_detected')
 
 
-def main():
-    depth = 16
-    k = 8
-    margin = 0.4
 
-    # for face detection
-    detector = dlib.get_frontal_face_detector()
+depth = 16
+k = 8
+margin = 0.4
 
-    # load model and weights
-    img_size = 256
-    temp = os.listdir("images")
-    temp.sort()
-    for fimg in temp:
-        print(fimg)
-        img = cv2.imread("images/"+fimg)
-        if not(img is None):
-          input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-          img_h, img_w, _ = np.shape(input_img)
+# for face detection
+detector = dlib.get_frontal_face_detector()
 
-          # detect faces using dlib detector
-          detected = detector(input_img, 1)
-          faces = np.empty((len(detected), img_size, img_size, 3))
+# load model and weights
+img_size = 256
+temp = os.listdir("images")
+temp.sort()
+for fimg in temp:
+print(fimg)
+img = cv2.imread("images/"+fimg)
+if not(img is None):
+  input_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+  img_h, img_w, _ = np.shape(input_img)
 
-          if len(detected) > 0:
-              for i, d in enumerate(detected):
-                  x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
-                  xw1 = max(int(x1 - margin * w), 0)
-                  yw1 = max(int(y1 - margin * h), 0)
-                  xw2 = min(int(x2 + margin * w), img_w - 1)
-                  yw2 = min(int(y2 + margin * h), img_h - 1)
-                  faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
-                  cv2.imwrite("mathais_face_detected/"+str(i)+'_'+fimg, faces[i, :, :, :])
+  # detect faces using dlib detector
+  detected = detector(input_img, 1)
+  faces = np.empty((len(detected), img_size, img_size, 3))
 
-if __name__ == '__main__':
-    main()
-
-
+  if len(detected) > 0:
+      for i, d in enumerate(detected):
+	  x1, y1, x2, y2, w, h = d.left(), d.top(), d.right() + 1, d.bottom() + 1, d.width(), d.height()
+	  xw1 = max(int(x1 - margin * w), 0)
+	  yw1 = max(int(y1 - margin * h), 0)
+	  xw2 = min(int(x2 + margin * w), img_w - 1)
+	  yw2 = min(int(y2 + margin * h), img_h - 1)
+	  faces[i, :, :, :] = cv2.resize(img[yw1:yw2 + 1, xw1:xw2 + 1, :], (img_size, img_size))
+	  cv2.imwrite("mathais_face_detected/"+str(i)+'_'+fimg, faces[i, :, :, :])
 
 """# Step 3.1 - First(Pseudo) part of the network (face Detector)
 
